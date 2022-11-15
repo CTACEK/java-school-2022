3e1765a0f02e3b3dd3ca7edd9131f3a02732a459
ctacek <mrusboy@mail.ru>
2022-11-15 15:25:42 +0300

    update task11

--- src/ru/croc/task11/AuctionLot.java
+++ src/ru/croc/task11/AuctionLot.java
@@ -1,19 +1,20 @@
 package ru.croc.task11;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class AuctionLot {
 
     private volatile String lastPersonName;
     private final LocalDateTime dateTimeEnd;
-    private volatile int actualPrice;
+    private volatile BigDecimal actualPrice;
 
     public AuctionLot(LocalDateTime dateTimeEnd) {
         this.dateTimeEnd = dateTimeEnd;
     }
 
-    public void setActualPrice(int actualPrice, String lastPersonName) {
-        if (this.actualPrice < actualPrice && LocalDateTime.now().isBefore(dateTimeEnd)) {
+    public void setActualPrice(BigDecimal actualPrice, String lastPersonName) {
+        if (this.actualPrice.doubleValue() < actualPrice.doubleValue() && LocalDateTime.now().isBefore(dateTimeEnd)) {
             this.actualPrice = actualPrice;
             this.lastPersonName = lastPersonName;
         }
