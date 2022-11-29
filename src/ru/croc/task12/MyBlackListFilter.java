1370d9a30cd9d02a7649a3d7530aba8072886cd8
ctacek <mrusboy@mail.ru>
2022-11-29 12:30:50 +0300

    finish task12 v2.0

--- src/ru/croc/task12/MyBlackListFilter.java
+++ src/ru/croc/task12/MyBlackListFilter.java
@@ -1,21 +1,19 @@
 package ru.croc.task12;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
 public class MyBlackListFilter implements BlackListFilter {
     @Override
     public void filterComments(List<String> comments, Set<String> blackList) {
-        List<String> oldComments = List.copyOf(comments);
-        comments.clear();
-
-        for (String oldComment : oldComments) {
-            String[] wordsOfOldComment = oldComment.split("[{.,!?\\s]+");
-            if (!wordsContainsInBlackList(wordsOfOldComment, blackList)) comments.add(oldComment);
+        Iterator<String> stringIterator = comments.iterator();
+        while (stringIterator.hasNext()) {
+            String[] words0fComment = stringIterator.next().split("[{.,!?\\s]+");
+            if (wordsContainsInBlackList(words0fComment, blackList)) stringIterator.remove();
         }
     }
-
-    public boolean wordsContainsInBlackList(String[] words, Set<String> blackList){
+    public boolean wordsContainsInBlackList(String[] words, Set<String> blackList) {
         for (String word : words) {
             if (blackList.contains(word.toLowerCase())) return true;
         }
